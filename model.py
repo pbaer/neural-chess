af073f8e68e83386b01c931ca18c57d1d50c38ec
Peter Baer <pbaer@microsoft.com>
2017-10-29 00:49:57 -0700

    Add synchronization of models in Azure blob storage.

--- model.py
+++ model.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
+from azure.storage.blob import BlockBlobService
+from azure.storage.blob import ContentSettings
 import keras
 from keras.backend.tensorflow_backend import set_session
 from keras.layers import Dense, Activation
 from keras.models import Sequential
+import os
 import tensorflow as tf
 
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.4
-set_session(tf.Session(config=config))
+# Use this to prevent 100% GPU memory usage
+#config = tf.ConfigProto()
+#config.gpu_options.per_process_gpu_memory_fraction = 0.4
+#set_session(tf.Session(config=config))
 
 def create_model():
     model = Sequential([
@@ -52,3 +56,52 @@ def save_model(model, filename):
     model_json = model.to_json()
     with open(filename + '.json', 'w') as json_file:
         json_file.write(model_json)
+
+def create_blob_service():
+    account_name = None
+    account_key = None
+    with open('.azurekey.txt') as file:
+        account_name = file.readline().strip()
+        account_key = file.readline().strip()
+    return BlockBlobService(account_name=account_name, account_key=account_key)
+
+def enumerate_local_models(filename_root=''):
+    local_models = []
+    for filename in os.listdir('model'):
+        if not filename.startswith(filename_root) or not filename.endswith('.json'):
+            continue
+        local_models.append('model/' + filename)
+    local_models.sort()
+    return local_models
+
+def enumerate_remote_models(blob_service, filename_root=''):
+    blob_models = []
+    blobs = blob_service.list_blobs('neural-chess')
+    for blob in blobs:
+        if not blob.name.startswith('model/' + filename_root) or not blob.name.endswith('.json'):
+            continue
+        blob_models.append(blob.name)
+    blob_models.sort()
+    return blob_models
+
+def upload_blob_model(blob_service, json_filename):
+    h5_filename = json_filename[:-5] + '.h5'
+    blob_service.create_blob_from_path('neural-chess', json_filename, json_filename, content_settings=ContentSettings(content_type='application/json'))
+    blob_service.create_blob_from_path('neural-chess', h5_filename, h5_filename, content_settings=ContentSettings(content_type='application/octet-stream'))
+
+def download_blob_model(blob_service, json_filename):
+    h5_filename = json_filename[:-5] + '.h5'
+    blob_service.get_blob_to_path('neural-chess', json_filename, json_filename)
+    blob_service.get_blob_to_path('neural-chess', h5_filename, h5_filename)
+
+def synchronize_blob_models(blob_service):
+    local_models = enumerate_local_models()
+    remote_models = enumerate_remote_models(blob_service)
+    for local in local_models:
+        if local not in remote_models:
+            print("Uploading %s..." % local)
+            upload_blob_model(blob_service, local)
+    for remote in remote_models:
+        if remote not in local_models:
+            print("Downloading %s..." % remote)
+            download_blob_model(blob_service, remote)
