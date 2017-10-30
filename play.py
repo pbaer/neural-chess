0248b06b2131b689f35eda6366c4b7fa5cf94beb
Peter Baer <pbaer@microsoft.com>
2017-10-29 17:25:48 -0700

    Begin to enable multithreaded play, not working yet.

--- play.py
+++ play.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 import chess
 import matplotlib.pyplot as plt
+from model import load_model
+from multiprocessing.dummy import Pool as ThreadPool
 import numpy as np
 import os
-#import random
-import sys
-import time
 from parse import featurize_board
-from model import load_model
+#import random
+from stockfish import Stockfish
+import sys
+import tensorflow as tf
+import time
 
 #CONFIDENCE_BIAS = 5
 
@@ -21,13 +24,32 @@ def init_stats():
     stats['results']['1-0'] = 0
     stats['results']['0-1'] = 0
     stats['results']['1/2-1/2'] = 0
-    stats['end_states'] = {}
+    #stats['end_states'] = {} # Skip tracking unique end states (TODO: add this back as an option)
     stats['minutes_elapsed'] = 0
-    stats['won_games'] = []
-    stats['draw_games'] = []
+    #stats['won_games'] = [] # Skip tracking won/draw games (TODO: add this back as an option)
+    #stats['draw_games'] = []
     stats['en_passant_captures'] = 0
     stats['castles'] = 0
     return stats
+
+def merge_stats(stats_list):
+    merged_stats = init_stats()
+    for stats in stats_list:
+        merged_stats['legal_moves'] += stats['legal_moves']
+        merged_stats['illegal_moves'] += stats['illegal_moves']
+        merged_stats['turns'] += stats['turns']
+        merged_stats['games'] += stats['games']
+        merged_stats['results']['1-0'] += stats['results']['1-0']
+        merged_stats['results']['0-1'] += stats['results']['0-1']
+        merged_stats['results']['1/2-1/2'] += stats['results']['1/2-1/2']
+        # merged_stats['end_states']
+        if (stats['minutes_elapsed'] > merged_stats['minutes_elapsed']):
+            merged_stats['minutes_elapsed'] = stats['minutes_elapsed']
+        # merged_stats['won_games']
+        # merged_stats['draw_games']
+        merged_stats['en_passant_captures'] += stats['en_passant_captures']
+        merged_stats['castles'] += stats['castles']
+    return merged_stats
 
 def print_intragame_stats(stats, prefix=''):
     moves = stats['legal_moves'] + stats['illegal_moves']
@@ -40,12 +62,27 @@ def print_stats(stats, prefix=''):
     print(prefix + ("%.1f minutes (%.2f seconds per game)" % (stats['minutes_elapsed'], 60 * stats['minutes_elapsed']/games)))
     print_intragame_stats(stats, prefix)
     print(prefix + ("%d turns (%.1f per game)" % (stats['turns'], stats['turns']/games)))
-    print(prefix + ("%d games (%.2f%% won, %.2f%% draw, %.2f%% lost, %.2f%% unique end states)" %
+    print(prefix + ("%d games (%.2f%% won, %.2f%% draw, %.2f%% lost)" %
          (games,
           100 * stats['results']['1-0']/games,
           100 * stats['results']['1/2-1/2']/games,
-          100 * stats['results']['0-1']/games,
-          100 * len(stats['end_states'])/games)))
+          100 * stats['results']['0-1']/games)))
+          #100 * len(stats['end_states'])/games)))
+
+def init_stats_logfile(filename):
+    with open(filename, 'w') as logfile:
+        logfile.write("model\tillegal_move_pct\twon_pct\tdraw_pct\tlost_pct\r\n")
+
+def print_stats_to_logfile(model_filename, stats, filename):
+    moves = stats['legal_moves'] + stats['illegal_moves']
+    games = stats['games']
+    with open(filename, 'a') as logfile:
+        logfile.write("%s\t%.2f%%\t%.2f%%\t%.2f%%\t%.2f%%\r\n" %
+                      (model_filename,
+                       100 * stats['illegal_moves']/moves,
+                       100 * stats['results']['1-0']/games,
+                       100 * stats['results']['1/2-1/2']/games,
+                       100 * stats['results']['0-1']/games))
 
 def generate_model_move(model, board, stats, show_output=False):
     assert board.turn # Must be White's turn
@@ -94,60 +131,81 @@ def play_interactive(model, board, move_uci=None):
     board.push(generate_model_move(model, board, init_stats(), show_output=True))
     return board
 
-def play_engine(model, engine, limit=1):
+# See https://github.com/fchollet/keras/issues/2397
+graph = None
+
+def play_engine(model, limit=10):
+    global graph
     stats = init_stats()
     start_time = time.time()
-    while limit > 0:
-        board = chess.Board()
-        engine.newgame()
-        game_node = chess.pgn.Game()
-        game_node.headers['White'] = 'Neural-Chess'
-        game_node.headers['Black'] = 'Stockfish'
-        while True:
-            if board.is_game_over():
-                stats['games'] += 1
-                stats['results'][board.result()] += 1
-                end_state = board.fen().split()[0] # Only consider the board itself, not castling rights, turn count etc.
-                if end_state in stats['end_states']:
-                    stats['end_states'][end_state] += 1
-                else:
-                    stats['end_states'][end_state] = 1
-                if board.result() == '1-0':
-                    stats['won_games'].append(str(game_node.root()))
-                    sys.stdout.write('X')
-                elif board.result() == '1/2-1/2':
-                    stats['draw_games'].append(str(game_node.root()))
-                    sys.stdout.write('-')
-                else:
-                    sys.stdout.write('.')
-                break
-            if board.turn:
-                move = generate_model_move(model, board, stats)
-            else:
-                move = generate_engine_move(engine, board)
-            board.push(move)
-            game_node = game_node.add_main_variation(move)
-            stats['turns'] += 1
-        limit -= 1
+    with graph.as_default():
+        with Stockfish(depth=0, param={'Skill Level':0}) as engine:
+            while limit > 0:
+                board = chess.Board()
+                engine.newgame()
+                game_node = chess.pgn.Game()
+                game_node.headers['White'] = 'Neural-Chess'
+                game_node.headers['Black'] = 'Stockfish'
+                while True:
+                    if board.is_game_over():
+                        stats['games'] += 1
+                        stats['results'][board.result()] += 1
+                        #end_state = board.fen().split()[0] # Only consider the board itself, not castling rights, turn count etc.
+                        #if end_state in stats['end_states']:
+                        #    stats['end_states'][end_state] += 1
+                        #else:
+                        #    stats['end_states'][end_state] = 1
+                        if board.result() == '1-0':
+                            #stats['won_games'].append(str(game_node.root()))
+                            sys.stdout.write('X')
+                        elif board.result() == '1/2-1/2':
+                            #stats['draw_games'].append(str(game_node.root()))
+                            sys.stdout.write('-')
+                        else:
+                            sys.stdout.write('.')
+                        break
+                    if board.turn:
+                        move = generate_model_move(model, board, stats)
+                    else:
+                        move = generate_engine_move(engine, board)
+                    board.push(move)
+                    game_node = game_node.add_main_variation(move)
+                    stats['turns'] += 1
+                limit -= 1
     stats['minutes_elapsed'] += (time.time() - start_time)/60
-    print()
-    print_stats(stats)
+    sys.stdout.write('!')
     return stats
 
-def play_engine_forever(model_filename_root, engine, limit):
-    last_model_filename = None
+def play_engine_forever(model_filename_root, last_model_filename=None):
+    global graph
+    thread_pool = ThreadPool(4)
     while os.path.isfile('.stopplay') == False:
         model_filename = None
+        seen_last_model_filename = False
         for filename in os.listdir('model'):
             if not filename.startswith(model_filename_root) or not filename.endswith('.json'):
                 continue
-            model_filename = filename
-        if model_filename == None or model_filename == last_model_filename:
+            if filename == last_model_filename:
+                seen_last_model_filename = True
+                continue
+            if seen_last_model_filename:
+                model_filename = filename # This is the first file after the last one we played
+                break
+        if model_filename == None:
             time.sleep(5)
             continue
         model = load_model('model/' + model_filename)
         print()
         print("Playing %s..." % model_filename)
-        play_engine(model, engine, limit)
+        model._make_predict_function() # have to initialize before threading
+        graph = tf.get_default_graph()
+        # TODO: for some reason the above doesn't actually work, it seems the predict
+        # function is regenerated during thread execution anyway
+        #all_stats = thread_pool.map(play_engine, [model, model, model, model])
+        stats = play_engine(model, 1000)
+        print()
+        #stats = merge_stats(all_stats)
+        print_stats(stats)
+        print_stats_to_logfile(model_filename, stats, '.playstats.txt')
         last_model_filename = model_filename
     os.remove('.stopplay')
